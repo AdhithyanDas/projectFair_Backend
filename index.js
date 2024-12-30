0ec33cfe9ceb55090f1ef73f9d4a4e8e93a205bf
AdhithyanDas <adhithyandask@gmail.com>
2024-12-30 23:48:16 +0530

    completed

--- index.js
+++ index.js
@@ -3,25 +3,17 @@ const express = require('express')
 const cors = require('cors')
 const route = require('./Routes/routes')
 require('./Connection/db')
-// const jwt = require('./Middlewares/jwtMiddleware')
 
 const pfServer = express()
 
-//configuring cors machanism in server
 pfServer.use(cors())
-//configuring json middleware to server to convert data from json to native
 pfServer.use(express.json())
-
-// pfServer.use(jwt)
-
-//configuring routes into server
 pfServer.use(route)
 
-pfServer.use('/Uploads',express.static('./Uploads'))
+pfServer.use('/Uploads', express.static('./Uploads'))
 
 const PORT = 3000 || process.env.PORT
 
 pfServer.listen(PORT, () => {
     console.log(`Server running at ${PORT}`);
 })
-
